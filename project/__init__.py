994784bb125ceb781fd73d1dd83ae77706717a8f
Joel <japoncea@gmail.com>
2017-03-01 09:02:59 -0800

    only config the db once

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,6 @@ from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/freedomsongs-db'
 app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
