5fd3c8af96abbc6e31b22895a589c2a6634164da
Joel <japoncea@gmail.com>
2017-03-01 08:21:39 -0800

    updating app.py

--- project/__init__.py
+++ project/__init__.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_modus import Modus
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/freedomsongs-db'
 app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SECRET_KEY'] = 'super secret'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 modus = Modus(app)
 bcrypt = Bcrypt(app)
