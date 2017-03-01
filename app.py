dba896b59780b956cc102b27e2d33e6d83d54b81
Joel <japoncea@gmail.com>
2017-03-01 08:05:51 -0800

    updating env

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from project import app
+import os
+
+if os.environ.get('ENV') == 'production':
+    debug = False
+else:
+    debug = True
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=debug)
